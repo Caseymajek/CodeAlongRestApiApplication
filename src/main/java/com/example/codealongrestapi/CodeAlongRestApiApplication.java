a13e3c11146b38c63a5dca34bea2f830a3958d2a
Caseymajek <taiwo.majolagbe@decagon.dev>
2024-01-17 12:43:04 +0100

    Setting up your CI/CD Pipeline

---
+++ src/main/java/com/example/codealongrestapi/CodeAlongRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.codealongrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeAlongRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeAlongRestApiApplication.class, args);
+    }
+
+}
